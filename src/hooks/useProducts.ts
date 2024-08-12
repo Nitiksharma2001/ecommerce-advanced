d1dea3ec5191f2794c4ccc320c9c9699e1dde349
Nitik Sharma <nitiksharma.job@gmail.com>
2024-08-13 03:16:10 +0530

    typescript fixed

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import type { ProductType } from '../types/products'
 import { useSearchParams } from 'react-router-dom'
 
